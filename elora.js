d81ae6bdfa332559ca753e3306bb3038dfcfd542
eloradosi <eloradosi46@gmail.com>
2023-12-14 09:28:29 +0700

    initial commit

---
+++ elora.js
@@ -0,0 +1,105 @@
+
+/* //soal 1
+let a = 1
+let b = 2
+let penjumlahan = a+b;
+console.log(penjumlahan)
+
+//soal 2
+let grade = 3.5;
+let nilai;
+if (grade >= 3.5){
+    nilai ="Cumlaude"
+} else if (grade < 3.5 && grade >= 3 ){
+    nilai ="Baik"
+} else if (grade <3 ){
+    nilai ="Cukup"
+}
+console.log(nilai); */
+
+//let array2 = [40,20,10,25,79,54]
+
+//let array = ["anggi", "krisna", "lala"]
+
+//console.log(array[array.length-1]);
+
+//--make string
+//console.log(array2.toString())
+//console.log(typeof array2[2].toString())
+
+//--pop = remove the last element
+//console.log(array2.pop())
+//console.log(array2)
+
+//--push = add new element to array at the end
+//console.log(array2.push(6))
+//console.log(array2)
+
+//--shift = remove first element
+//console.log(array2.shift())
+//console.log(array2)
+
+//--unshift = add new element to array at the beginning 
+//console.log(array2.unshift(0))
+//console.log(array2)
+
+//--sort = rearrange array
+//console.log(array2.sort())
+//console.log(array2)
+
+//--sort for number from a-z
+//console.log(array2.sort(function(a,b) {return a-b}))
+
+//--sort for number from z-a
+//console.log(array2.sort(function(a,b) {return b-a }))
+
+//--reverse = reverse sort
+//console.log(array2.sort())
+//console.log(array2.reverse())
+
+/*function sorting(arr) {
+    let length = arr.length;
+    
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1; j++) {
+        // Compare adjacent elements
+        if (arr[j] > arr[j + 1]) {
+          // Swap elements if they are in the wrong order
+          var temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  
+    return arr;
+  }
+  
+  let numbers = [4, 2, 7, 1, 9, 5];
+  let sortedNumbers = sorting(numbers);
+  console.log(sortedNumbers);
+  */
+  
+ /* let a = 2
+  let b = 3
+
+  console.log((a++))
+  console.log(a) 
+  */
+ 
+ // console.log(true ? '1' : '2')
+  
+/*
+=======================================
+DAY-8
+JAVASCRIPT ALGORITHM & DECISION MAKING
+13 DESEMBER 2023
+=======================================
+*/
+
+
+
+fetch().then()
+
+
+
